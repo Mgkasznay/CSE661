2ee8d2b9ba383a0879c0be2d7ef3ce7f24b1f58e
Mgkasznay <madeline@kasznay.com>
2025-07-28 22:12:04 -0400

    Added initial grab of command line arguments

--- main.cpp
+++ main.cpp
@@ -4,14 +4,48 @@
 
 using namespace std;
 
+bool program_running = 1;
+
 int main()
 {
    
-   vector<string> msg {"Hello", "CLASS", "For", "CSE661/CSI655", "and Homework 2!"};
+   while(program_running){
 
-   for (const string& word : msg)
-   {
-      cout << word << " ";
+      //Starting Hello World Statement Remove Later
+      vector<string> msg {"Hello", "CLASS", "For", "CSE661/CSI655", "and Homework 2!"};
+
+      for (const string& word : msg)
+      {
+         cout << word << " ";
+      }
+      cout << endl;
+
+      //Initial grab of the arguments
+      string opcode;
+      string regA;
+      string regB;
+      string regC;
+      string imed;
+
+      cout << "> ";
+      cin >> opcode;
+      cout << "Opcode: " << opcode << endl; //Will replace with more useful code?
+
+      //Could add a make lower case here for ease of use
+
+      if(opcode == "add" | opcode == "sub"){
+
+         cin >> regA >> regB >> regC;
+
+         cout << "RegA: " << regA << endl; //Will Replace later
+         cout << "RegB: " << regB << endl;
+         cout << "RegC: " << regC << endl;
+
+      }
+      //Will add more cases of commands with different arguements below :)
+
+      //Will be used to be the program running until halt add later
+      program_running = 0;
+
    }
-   cout << endl;
 }
